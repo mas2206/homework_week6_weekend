fd257c14506c063c896d26a598b93f4d13b3c353
Michael Shennan <m.shennan22@gmail.com>
2017-04-16 17:53:09 +0100

    cant get win checker to work

--- card_game/WinChecker.java
+++ card_game/WinChecker.java
@@ -5,4 +5,12 @@ import java.util.*;
 
 public class WinChecker {
 
+  // public String highCardWinner(){
+  //   if(player1.getCard()[0].number > player2.getCard()[0].number){
+  //     return "Player 1 wins";
+  //   }else {
+  //     return "Player 2 wins";
+  //   }
+  // }
+
 }
--- card_tests/WinCheckerTest.java
+++ card_tests/WinCheckerTest.java
@@ -6,4 +6,26 @@ import java.util.*;
 
 public class WinCheckerTest {
 
+  // Game game;
+  // Player player1;
+  // Player player2;
+  // Card card1;
+  // Card card2;
+
+  // @Before
+  // public void before(){
+  //   game = new Game();
+  //   player1 = new Player("Phil");
+  //   player2 = new Player("Tom");
+  //   card1 = new Card(6, Suit.Clubs);
+  //   card2 = new Card(10, Suit.Hearts);
+  //   player1.setCard(card1);
+  //   player2.setCard(card2);
+  // }
+
+  // @Test
+  // public void tenBeatsSix(){
+  //   assertEquals("Player 2 wins", game.highCardWinner());
+  // }
+
 }
